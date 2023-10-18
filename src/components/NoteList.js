c0eaee9f53ac8f3e7dc64efdad47f95571f487e2
+ <+>
2023-10-18 09:22:11 -0400

    save

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -3,7 +3,13 @@ import NoteItem from "./NoteItem";
 import Sidebar from "./Sidebar";
 
 function NoteList({allNotes}) {
-  const renderAllNotes = allNotes.map((note) => <NoteItem key={note.id} body={note.body} title={note.title} userId={note.userId} />)
+  const renderAllNotes = allNotes.map((note) => 
+  <NoteItem 
+  key={note.id} 
+  body={note.body} 
+  title={note.title} 
+  userId={note.userId} 
+  />)
 
   return (
     <ul>
